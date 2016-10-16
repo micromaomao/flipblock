9d0b9995d650cabc953c4465bcf657380d6e0b4d
Mao Wtm <micromaomao@gmail.com>
2016-10-16 11:03:05 +0800

    Add example level

--- levels.js
+++ levels.js
@@ -16,6 +16,22 @@ const FlipBlock_Levels = [
     fb.buildGroundTarget(1, 1)
   },
   fb => {
-    fb.groundDrawRect(0, 0, 3, 3)
+    fb.groundDrawRect(0, 0, 1, 3)
+    fb.groundDrawRect(1, 2, 2, 1)
+    fb.buildGroundBlock(2, 0)
+    fb.buildGroundBlock(3, 1)
+    fb.buildGroundTarget(3, 2)
+  },
+  fb => {
+    fb.groundDrawRect(0, 0, 2, 5)
+    fb.groundEraseRect(1, 1, 1, 3)
+    fb.buildGroundTarget(1, 2)
+  },
+  fb => {
+    fb.groundDrawRect(0, 0, 15, 15)
+    fb.groundEraseRect(3, 2, 1, 5)
+    fb.groundEraseRect(3, 2, 3, 1)
+    fb.groundEraseRect(3, 4, 2, 1)
+    fb.groundEraseRect(3, 6, 3, 1)
   }
 ]
