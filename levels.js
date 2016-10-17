c2a1159c75f3f9ea366595d8b346b033b1836400
Mao Wtm <micromaomao@gmail.com>
2016-10-17 21:19:21 +0800

    Add more levels

--- levels.js
+++ levels.js
@@ -1,6 +1,9 @@
 const Colors = {
   FAIL_ON_STAND: 0x222222,
-  VANISH_ON_TOUCH: 0xf1ca4f
+  VANISH_ON_TOUCH: 0xf1ca4f,
+  SUCCESS_ON_TOUCH: 0x09f7a0,
+  VANISH_ON_STAND_AND_SUCCESS_ON_STAND_ALL: 0x09d7f7,
+  TARGET: 0x4caf50
 }
 const FlipBlock_Levels = [
   fb => {
@@ -29,6 +32,10 @@ const FlipBlock_Levels = [
     fb.buildGroundTarget(1, 1)
   },
   fb => {
+    fb.groundDrawRect(0, 0, 2, 2)
+    fb.buildGroundTarget(0, 1)
+  },
+  fb => {
     fb.groundDrawRect(0, 0, 1, 3)
     fb.groundDrawRect(1, 2, 2, 1)
     fb.buildGroundBlock(2, 0)
@@ -49,6 +56,49 @@ const FlipBlock_Levels = [
   fb => {
     fb.buildGroundBlock(0, 0)(1, 0)(1, 1)(0, 2)(2, 0)
     fb.buildGroundTarget(2, 2)
+  },
+  function* (fb) {
+    fb.groundDrawRect(0, 0, 5, 3)
+    fb.groundEraseRect(1, 1, 3, 1)
+    fb.buildGroundTarget(2, 2)
+    let vanish = {color: Colors.VANISH_ON_TOUCH}
+    fb.buildGroundBlock(0, 1, vanish)(0, 2, vanish)(1, 2, vanish)
+    while (true) {
+      if (fb._transforming) {
+        yield
+        continue
+      }
+      let fd = fb.getDownFaceCoords()
+      fd.forEach(([x, y, v]) => {
+        if (v && v.color === Colors.VANISH_ON_TOUCH) {
+          fb.clearGroundBlock(x, y)
+        }
+      })
+      yield
+    }
+  },
+  function* (fb) {
+    fb.groundDrawRect(0, 0, 5, 4)
+    let vanish = {color: Colors.VANISH_ON_TOUCH}
+    fb.groundDrawRect(0, 1, 1, 2, vanish)
+    fb.groundDrawRect(1, 3, 3, 1, vanish)
+    fb.groundDrawRect(3, 0, 2, 2, vanish)
+    fb.clearGroundBlock(2, 2)
+    fb.buildGroundBlock(1, 0, vanish)(1, 1, vanish)(2, 0, vanish)
+    fb.buildGroundTarget(2, 1)
+    while (true) {
+      if (fb._transforming) {
+        yield
+        continue
+      }
+      let fd = fb.getDownFaceCoords()
+      fd.forEach(([x, y, v]) => {
+        if (v && v.color === Colors.VANISH_ON_TOUCH) {
+          fb.clearGroundBlock(x, y)
+        }
+      })
+      yield
+    }
   },
   fb => {
     fb.buildGroundBlock(0, 0)(0, 1)
@@ -81,29 +131,9 @@ const FlipBlock_Levels = [
     }
   },
   function* (fb) {
-    fb.groundDrawRect(0, 0, 5, 3)
-    fb.groundEraseRect(1, 1, 3, 1)
-    fb.buildGroundTarget(2, 2)
-    let vanish = {color: Colors.VANISH_ON_TOUCH}
-    fb.buildGroundBlock(0, 1, vanish)(0, 2, vanish)(1, 2, vanish)
-    while (true) {
-      if (fb._transforming) {
-        yield
-        continue
-      }
-      let fd = fb.getDownFaceCoords()
-      fd.forEach(([x, y, v]) => {
-        if (v && v.color === Colors.VANISH_ON_TOUCH) {
-          fb.clearGroundBlock(x, y)
-        }
-      })
-      yield
-    }
-  },
-  function* (fb) {
     fb.groundDrawRect(0, 0, 10, 10, {color: Colors.VANISH_ON_TOUCH})
     fb.buildGroundTarget(9, 8)
-    while(true) {
+    while (true) {
       if (fb._transforming) {
         yield
         continue
@@ -114,6 +144,52 @@ const FlipBlock_Levels = [
         if (x === 9 && y === 8) return
         fb.clearGroundBlock(x, y)
       })
+      yield
+    }
+  },
+  function* (fb) {
+    fb.groundDrawRect(0, 0, 4, 4)
+    let target = {color: Colors.VANISH_ON_STAND_AND_SUCCESS_ON_STAND_ALL}
+    fb.buildGroundBlock(1, 1, target)(2, 2, target)
+    let left = 2
+    while (true) {
+      if (fb._transforming) {
+        yield
+        continue
+      }
+      let wo = fb.getDownFaceCoords()
+      if (wo.length === 1) {
+        let tg = wo[0]
+        if (tg[2] && tg[2].color === Colors.VANISH_ON_STAND_AND_SUCCESS_ON_STAND_ALL) {
+          fb.clearGroundBlock(tg[0], tg[1])
+          left --
+        }
+      }
+      if (left === 0) {
+        fb.success()
+      }
+      yield
+    }
+  },
+  function* (fb) {
+    fb.groundDrawRect(0, 0, 10, 10, {color: Colors.SUCCESS_ON_TOUCH})
+    let left = 10 * 10
+    while (true) {
+      if (fb._transforming) {
+        yield
+        continue
+      }
+      let wo = fb.getDownFaceCoords()
+      wo.forEach(v => {
+        let [x, y, b] = v
+        if (b) {
+          fb.clearGroundBlock(x, y)
+          left --
+        }
+      })
+      if (left <= 0) {
+        fb.success()
+      }
       yield
     }
   },
